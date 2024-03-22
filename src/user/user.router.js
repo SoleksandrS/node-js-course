25be673ec5a72afc018722e858fb08706805432a
SoleksandrS <o.y.sitaylo@student.khai.edu>
2024-03-22 21:17:39 +0200

    feat: add route to get user`s products

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const { eq } = require('drizzle-orm');
 const db = require('../db');
-const { users } = require('../db/schema');
+const { users, products } = require('../db/schema');
 
 const router = express.Router();
 
 router.get('/users', async (request, response) => {
   const users = await db.query.users.findMany();
   return response.json(users);
+});
+
+router.get('/users/:id/products', async (request, response) => {
+  const { id } = request.params;
+  const userProducts = await db.query.products.findMany({
+    where: eq(products.userId, +id)
+  });
+  return response.json(userProducts);
 });
 
 router.post('/users', async (request, response) => {
